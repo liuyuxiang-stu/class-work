7833b249890825c3ec60a2340ed2424dfc272c4f
liuyuxiang-stu <2777621634@qq.com>
2026-03-19 23:58:06 +0800

    ex03 final version

--- ex03/ex03.ino
+++ ex03/ex03.ino
@@ -1,57 +1,94 @@
 #define LED1  2
 #define LED2  5
 #define LED3  23
+
 int fre = 10;
 int resolution = 8;
 unsigned long starttime = 0;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   ledcAttach(LED1, fre, resolution);
   ledcAttach(LED2, fre, resolution);
   ledcAttach(LED3, fre, resolution);
+  ledcWrite(LED1, 0);
+  ledcWrite(LED2, 0);
+  ledcWrite(LED3, 0);
 }
-void shortshine(int a,int b,int c)
+void d(int a)
 {
-    for(int dutycycle=0;dutycycle<=255;dutycycle+=2)
-    {ledcWrite(a,dutycycle); 
-     ledcWrite(b,dutycycle); 
-     ledcWrite(c,dutycycle);
-    }
-    for(int dutycycle=255;dutycycle>=0;dutycycle-=2)
-    {ledcWrite(a,dutycycle);
-     ledcWrite(b,dutycycle); 
-     ledcWrite(c,dutycycle);
-    }
-  starttime=millis();
+  unsigned long now = millis();
+  while (1)
+  {
+    if(millis() - now >= 1000)break;
   }
-
-
-void longshine(int a,int b,int c)
+}
+void shortd(void)
 {
-    for(int dutycycle=0;dutycycle<=255;dutycycle++)
-    {ledcWrite(a,dutycycle); 
-     ledcWrite(b,dutycycle); 
-     ledcWrite(c,dutycycle);
-    }
-    for(int dutycycle=255;dutycycle>=0;dutycycle--)
-    {ledcWrite(a,dutycycle);
-     ledcWrite(b,dutycycle); 
-     ledcWrite(c,dutycycle);
-    }
-  starttime=millis();
+  unsigned long now = millis();
+  while (1)
+  {
+    if(millis() - now >= 100)break;
   }
+}
+void longd(void)
+{
+  unsigned long now = millis();
+  while (1)
+  {
+    if(millis() - now >= 200)break;
+  }
+}
+void longshine(int a, int b, int c)
+{
+  for(int i=1;i<=3;i++)
+  {
+    ledcWrite(a,255);
+    ledcWrite(b,255);
+    ledcWrite(c,255);
+    longd();
+    ledcWrite(a,0);
+    ledcWrite(b,0);
+    ledcWrite(c,0);
+    longd();
+  }
+  starttime = millis();
+}
+void shortshine(int a, int b, int c)
+{
+  for(int i=1;i<=3;i++)
+  {
+    ledcWrite(a,255);
+    ledcWrite(b,255);
+    ledcWrite(c,255);
+    shortd();
+
+    ledcWrite(a,0);
+    ledcWrite(b,0);
+    ledcWrite(c,0);
+    shortd();
+  }
+  starttime = millis();
+}
 
 void loop() {
-  // put your main code here, to run repeatedly:
   unsigned long time = millis();
   for(int i=3;i<=5;i++)
   {
-    if(i%2!=0) shortshine(LED1,LED2,LED3);
-    else longshine(LED1,LED2,LED3);
+    if(i%2!=0) {
+      shortshine(LED1, LED2, LED3);
+    } else {
+      longshine(LED1, LED2, LED3);
+    }
+
+    ledcWrite(LED1,0);
+    ledcWrite(LED2,0);
+    ledcWrite(LED3,0);
+    d(100);
   }
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED2,LOW);
-  digitalWrite(LED3,LOW);
-  if(time-starttime>=10000)starttime=time;
+
+  ledcWrite(LED1,0);
+  ledcWrite(LED2,0);
+  ledcWrite(LED3,0);
+  d(1000);
 }
